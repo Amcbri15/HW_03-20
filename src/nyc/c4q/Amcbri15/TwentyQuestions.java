ffe534dd69000f03a437e6f808d8587c52c2ff9f
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-03-18 19:08:36 -0400

    Added TwentyQuestions

---
+++ src/nyc/c4q/Amcbri15/TwentyQuestions.java
@@ -0,0 +1,56 @@
+package nyc.c4q.Amcbri15;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ * Created by c4q-anthony-mcbride on 3/18/15.
+ */
+public class TwentyQuestions {
+    public static void guessGame(){
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        int userGuess;
+        int chances = 0;
+
+        int number = rand.nextInt(100000);
+
+        //Debug mode
+        System.out.println(number);
+
+
+        System.out.print("I'm thinking of a number between 1 and 100,000. What is my number? ");
+        userGuess = in.nextInt();
+
+        while (userGuess != number) {
+            chances += 1;
+
+            if (userGuess < number) {
+                System.out.println("\nMy number is higher than " + userGuess + ".");
+                System.out.print("Your guess: ");
+                userGuess = in.nextInt();
+            }
+            else if(userGuess > number) {
+                System.out.println("\nMy number is lower than " + userGuess + ".");
+                System.out.print("Your guess: ");
+                userGuess = in.nextInt();
+            }
+            if (chances >= 20) {
+                System.out.println("I'm sorry. You're out of guesses... Game over!");
+                break;
+            }
+        }
+        if (userGuess == number) {
+            System.out.println("Correct! My number was:" + number + "\nYou win!");
+        }
+
+        //System.out.println("Number: " + number + "\nYour guess: " + userGuess);
+
+
+
+
+    }
+    public static void main(String[] args) {
+
+        guessGame();
+
+    }
+}
